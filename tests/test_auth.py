9b33a346d88715ae8e1b12d49869a14f73e2f80f
violetta <lotsmanovavioletta@gmail.com>
2024-02-04 15:19:33 +0300

    fixed test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -9,7 +9,7 @@ def auth_header():
 def test_register(test_client):
 
     # TestCase1 register user
-    response = test_client.post('/register', json={'username': 'testuser', 'password': 'testpass'})
+    response = test_client.post('/register', json={'username': 'test', 'password': 'testpass'})
     data = response.get_json()
     assert response.status_code == 200
     assert 'access_token' in data
@@ -18,7 +18,7 @@ def test_register(test_client):
 def test_login(test_client):
     global jwt_token
     # TestCase2 login user
-    response = test_client.post('/login', json={'username': 'testuser', 'password': 'testpass'})
+    response = test_client.post('/login', json={'username': 'test', 'password': 'testpass'})
     data = response.get_json()
 
     jwt_token = data['access_token']
