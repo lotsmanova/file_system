6cd8fd9d62225759650bd7af57fff6a8205faf28
violetta <lotsmanovavioletta@gmail.com>
2024-02-04 13:03:28 +0300

    add logging

--- src/main.py
+++ src/main.py
@@ -1,16 +1,14 @@
 import logging
+
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from src.config import SQLALCHEMY_DATABASE_URI, SECRET_AUTH, db
 from src.route.auth import route_auth
 from src.route.command import route_com
 
-# Настройка логгера
-logging.basicConfig(level=logging.DEBUG,
-                    format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
-
 
 def create_app():
+
     app = Flask(__name__)
 
     # Конфигурация приложения
@@ -22,8 +20,17 @@ def create_app():
 
     # Инициализация JWT
     jwt = JWTManager(app)
-    app.register_blueprint(route_auth)
+
+    # Настройка логгера
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s',
+                        handlers=[
+                            logging.FileHandler('app.log'),
+                            logging.StreamHandler()
+                        ])
+
     # Регистрация маршрутов
+    app.register_blueprint(route_auth)
     app.register_blueprint(route_com)
 
     return app
