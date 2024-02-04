1f6646b90e9a864b8e5a5270f9f650e35c284605
violetta <lotsmanovavioletta@gmail.com>
2024-02-04 12:21:18 +0300

    fixed main

--- src/main.py
+++ src/main.py
@@ -1,26 +1,34 @@
+import logging
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from src.config import SQLALCHEMY_DATABASE_URI, SECRET_AUTH, db
+from src.route.auth import route_auth
 from src.route.command import route_com
 
-# create app
-app = Flask(__name__)
+# Настройка логгера
+logging.basicConfig(level=logging.DEBUG,
+                    format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
-# add config
-app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-app.config['JWT_SECRET_KEY'] = SECRET_AUTH
 
-# connect db
-db.init_app(app)
-# creat jwt
-jwt = JWTManager(app)
+def create_app():
+    app = Flask(__name__)
 
-# add route command
-app.register_blueprint(route_com)
+    # Конфигурация приложения
+    app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+    app.config['JWT_SECRET_KEY'] = SECRET_AUTH
+
+    # Инициализация базы данных
+    db.init_app(app)
+
+    # Инициализация JWT
+    jwt = JWTManager(app)
+    app.register_blueprint(route_auth)
+    # Регистрация маршрутов
+    app.register_blueprint(route_com)
+
+    return app
 
 
 if __name__ == '__main__':
-    from src.route.auth import route_auth
-    # add route auth
-    app.register_blueprint(route_auth)
+    app = create_app()
     app.run(debug=True)
